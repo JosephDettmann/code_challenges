fef47d04f0770e663706444defbe13bb9dc7707b
Joseph <JosephDettmann@gmail.com>
2023-04-28 17:22:26 -0500

    revised square.py and test_square.py using black

--- tests/test_square.py
+++ tests/test_square.py
@@ -2,8 +2,8 @@ from src.square import square
 
 
 def test_square():
-    assert(square(8) == 64)
-    assert(square(9) == 81)
-    assert(square(10) == 100)
-    assert(square(11) == 121)
-    assert(square(12) == 144)
+    assert square(8) == 64
+    assert square(9) == 81
+    assert square(10) == 100
+    assert square(11) == 121
+    assert square(12) == 144
