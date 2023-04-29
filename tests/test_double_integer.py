fe677752a9997d4a2917e5ffad13e6303cf81d7d
Joseph <JosephDettmann@gmail.com>
2023-04-29 18:47:19 -0500

    readded a multitude of files across all current branches, accidentally deleted test_double_integer.py, created a new test_double_integer.py file

--- tests/test_double_integer.py
+++ tests/test_double_integer.py
@@ -2,6 +2,8 @@ from src.double_integer import double_integer
 
 
 def test_double_integer():
-    assert(double_integer(5) == 10)
     assert(double_integer(3) == 6)
-    assert(double_integer(7) == 14)
+    assert(double_integer(6) == 12)
+    assert(double_integer(9) == 18)
+    assert(double_integer(12) == 24)
+    assert(double_integer(16) == 32)
