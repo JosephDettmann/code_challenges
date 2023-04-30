aaefc9e087bb3abe33f34ecbb803a89744cf6133
Joseph <JosephDettmann@gmail.com>
2023-04-30 09:49:17 -0500

    reformatted test_volume.py with black

--- tests/test_volume.py
+++ tests/test_volume.py
@@ -2,8 +2,8 @@ from src.volume import volume
 
 
 def test_volume():
-    assert(volume(4, 2, 3) == 24)
-    assert(volume(5, 5, 3) == 75)
-    assert(volume(5, 3, 4) == 60)
-    assert(volume(4, 3, 4) == 48)
-    assert(volume(1, 3, 17) == 51)
+    assert volume(4, 2, 3) == 24
+    assert volume(5, 5, 3) == 75
+    assert volume(5, 3, 4) == 60
+    assert volume(4, 3, 4) == 48
+    assert volume(1, 3, 17) == 51
