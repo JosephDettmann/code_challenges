e7c8751fc407c6953a8f19d27149b6a0e88fd4ca
Joseph <JosephDettmann@gmail.com>
2023-05-02 19:19:39 -0500

    reformatted invert_values.py and its test using black, fixed third assertion in test_invert_values.py

--- src/invert_values.py
+++ src/invert_values.py
@@ -1,5 +1,6 @@
-def invert_values(lst):
-    return [-x for x in lst]
+def invert_values(nums):
+    return [-x for x in nums]
+
 
 if __name__ == "__main__":
     print(invert_values([1, -2, 3, -4, 5]))
