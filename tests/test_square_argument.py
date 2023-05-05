9d2ffa852d35aac71094e3762bf473452b84b798
Joseph <JosephDettmann@gmail.com>
2023-05-05 07:06:14 -0500

    added square_argument and the test for it, ran black and checked files with python and pytest.

---
+++ tests/test_square_argument.py
@@ -0,0 +1,11 @@
+from src.square_argument import square_argument
+
+
+def test_square_argument():
+    assert square_argument(7) == 49
+    assert square_argument(6) == 36
+    assert square_argument(5) == 25
+    assert square_argument(4) == 16
+    assert square_argument(3) == 9
+    assert square_argument(2) == 4
+    assert square_argument(2) != 2342342
