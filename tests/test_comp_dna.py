e2e0fd06a6a6728695d7cadbd8e64b5e9086c557
Joseph <JosephDettmann@gmail.com>
2023-05-07 07:02:38 -0500

    added assertion for 'ATCG'

--- tests/test_comp_dna.py
+++ tests/test_comp_dna.py
@@ -2,6 +2,7 @@ from src.comp_dna import complementary_dna
 
 
 def test_complementary_dna():
+    assert complementary_dna("ATCG") == "TAGC"
     assert complementary_dna("AAAA") == "TTTT"
     assert complementary_dna("TTTT") == "AAAA"
     assert complementary_dna("CCCC") == "GGGG"
