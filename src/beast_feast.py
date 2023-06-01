240498b34d8369b0f385476d0abc1c858c378192
Joseph <JosephDettmann@gmail.com>
2023-06-01 07:54:26 -0500

    ran black on beast_feast.py and its test file.

--- src/beast_feast.py
+++ src/beast_feast.py
@@ -1,5 +1,8 @@
 def beast_feast(beast, dish):
-    return beast[0].casefold() == dish[0].casefold()and dish[-1].casefold() == beast[-1].casefold()
+    return (
+        beast[0].casefold() == dish[0].casefold()
+        and dish[-1].casefold() == beast[-1].casefold()
+    )
 
 
 if __name__ == "__main__":
--- tests/test_beast_feast.py
+++ tests/test_beast_feast.py
@@ -4,4 +4,7 @@ from src.beast_feast import beast_feast
 def test_best_feast():
     assert beast_feast("Tuna", "tapioca") == True
     assert beast_feast("Octopus", "olives") == True
-    assert beast_feast("Muhammad ibn Musa al-Khwarizmi", "moderately difficult algebra")== False
+    assert (
+        beast_feast("Muhammad ibn Musa al-Khwarizmi", "moderately difficult algebra")
+        == False
+    )
