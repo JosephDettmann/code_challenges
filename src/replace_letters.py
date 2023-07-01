d81136fb2c5085f65a1cb114b5b2581b18628b0e
Joseph <JosephDettmann@gmail.com>
2023-07-01 11:54:25 -0500

    refactored replace_letters.py and its tests file, ran using black and checked using pytest.

--- src/replace_letters.py
+++ src/replace_letters.py
@@ -1,34 +1,4 @@
 def replace_letters(word):
-    word = word.casefold()
-    dictionary = {
-        "a": "z",
-        "b": "e",
-        "c": "e",
-        "d": "e",
-        "e": "d",
-        "f": "i",
-        "g": "i",
-        "h": "i",
-        "i": "h",
-        "j": "o",
-        "k": "o",
-        "l": "o",
-        "m": "o",
-        "n": "o",
-        "o": "n",
-        "p": "u",
-        "q": "u",
-        "r": "u",
-        "s": "u",
-        "t": "u",
-        "u": "t",
-        "v": "a",
-        "w": "a",
-        "x": "a",
-        "y": "a",
-        "z": "a",
-    }
-    new_word = ""
-    for char in word:
-        new_word += dictionary.get(char)
-    return new_word
+    return word.translate(
+        str.maketrans("abcdefghijklmnopqrstuvwxyz", "zeeediiihooooonuuuuutaaaaa")
+    )
--- tests/test_replace_letters.py
+++ tests/test_replace_letters.py
@@ -4,3 +4,4 @@ from src.replace_letters import replace_letters
 def test_replace_letters():
     assert replace_letters("hello") == "idoon"
     assert replace_letters("codewars") == "enedazuu"
+    assert replace_letters("abcdtuvwxyz") == "zeeeutaaaaa"
