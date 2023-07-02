eb18ce48350f2385f6026fd66f33b56b213b8db3
Joseph <JosephDettmann@gmail.com>
2023-07-02 10:54:35 -0500

    Added duplicate_count.py and it's test file, reformatted with black and ran with pytest.

---
+++ tests/test_duplicate_count.py
@@ -0,0 +1,8 @@
+from src.duplicate_count import duplicate_count
+
+
+def test_duplicate_count():
+    assert duplicate_count("") == 0
+    assert duplicate_count("abcde") == 0
+    assert duplicate_count("abcdeaB") == 2
+    assert duplicate_count("Indivisibilities") == 2
